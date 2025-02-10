5e0d18a13502508e4dad1f8c8cd2e0db667c309c
interstellarEMwave <ingemar@live.no>
2025-02-10 01:35:05 +0100

    Make outside wall special wall

--- main.c
+++ main.c
@@ -14,8 +14,6 @@
 
 typedef unsigned __int8 u8;
 
-//a
-
 typedef enum
 ENUM_cellType
 {
@@ -142,10 +140,117 @@ map_generateRooms(u8* map, int map_height, int map_width, int roomCountLower, in
 
 
 void
-print_bufferClear(u8* print_buffer)
+map_dfsFill(u8* map, int map_height, int map_width, int y, int x, char fill)
 {
-	for(int i = 0; i < PRINT_MAX_HEIGHT; ++i)
+	if(map[y*map_width + x] != EMPTY) return;
+	else
 	{
+		map[y*map_width + x] = fill;
+		map_dfsFill(map, map_height, map_width, max(y-1, 0),			x,					   fill);
+		map_dfsFill(map, map_height, map_width, min(y+1, map_height-1), x,					   fill);
+		map_dfsFill(map, map_height, map_width, y,						max(x-1, 0),		   fill);
+		map_dfsFill(map, map_height, map_width, y,						min(x+1, map_width-1), fill);
+	}
+}
+
+
+void
+map_fillRooms(u8* map, int map_height, int map_width)
+{
+	char fill = 'A';
+	for(int i = 0; i < map_height*map_width; ++i)
+	{
+		if(map[i] == EMPTY)
+		{
+			map_dfsFill(map, map_height, map_width, i/map_width, i%map_width,  fill);
+			++fill;
+		}
+	}
+}
+
+
+void
+map_addToOutsideAreas(u8 c, u8* outsideAreas, int* outsideAreasLength)
+{
+	char flag = 1;
+	for(int i = *outsideAreasLength - 1; i > -1; --i)
+	{
+		if(outsideAreas[i] == c)
+		{
+			flag = 0;
+			break;
+		}
+	}
+	if(flag)
+	{
+		outsideAreas[*outsideAreasLength] = c;
+		*outsideAreasLength += 1;
+	}
+}
+
+void
+map_makeDoorways(u8* map, int map_height, int map_width)
+{
+	u8 calcMap[MAP_MAX_HEIGHT * MAP_MAX_WIDTH] = {0};
+	memcpy(calcMap, map, MAP_MAX_HEIGHT*MAP_MAX_WIDTH);
+
+	map_fillRooms(calcMap, map_height, map_width);
+
+	u8 outsideAreas[200] = {0};
+	int outsideAreasLength = 0;
+	if(calcMap[0] != WALL)
+	{
+		outsideAreas[0] = calcMap[0];
+		++outsideAreasLength;
+	}
+	for(int i = 0; i < map_width; ++i)
+	{
+		if(calcMap[i] != WALL)							  map_addToOutsideAreas(calcMap[i],							   outsideAreas, &outsideAreasLength);
+		if(calcMap[(map_height-1)*map_width + i] != WALL) map_addToOutsideAreas(calcMap[(map_height-1)*map_width + i], outsideAreas, &outsideAreasLength);
+	}
+
+	for(int i = 1; i < map_height-1; ++i)
+	{
+		if(calcMap[i*map_width] != WALL)		 map_addToOutsideAreas(calcMap[i*map_width],		 outsideAreas, &outsideAreasLength);
+		if(calcMap[(i+1)*map_width - 1] != WALL) map_addToOutsideAreas(calcMap[(i-1)*map_width - 1], outsideAreas, &outsideAreasLength);
+	}
+
+	for(int i = 0; i < map_height*map_width; ++i)
+	{
+		for(int j = 0; j < outsideAreasLength; ++j)
+		{
+			if(calcMap[i] == outsideAreas[j]) calcMap[i] = EMPTY;
+		}
+	}
+
+	for(int i = 0; i < map_height*map_width; ++i)
+	{
+		if(calcMap[i] == WALL && 
+								 (i           < map_width
+							   || i/map_width == map_height-1
+							   || i%map_width == 0
+							   || i%map_width == map_width-1
+							   || calcMap[i-1] == EMPTY
+							   || calcMap[i+1] == EMPTY
+							   || calcMap[i-map_width] == EMPTY
+							   || calcMap[i+map_width] == EMPTY
+							   || calcMap[i-1-map_width] == EMPTY
+							   || calcMap[i+1-map_width] == EMPTY
+							   || calcMap[i-1+map_width] == EMPTY
+							   || calcMap[i+1+map_width] == EMPTY))
+		{
+			map[i] = FLOOR;
+		}
+	}
+
+	//TODO more shit here
+}
+			
+
+
+void
+print_bufferClear(u8* print_buffer)
+{ for(int i = 0; i < PRINT_MAX_HEIGHT; ++i) {
 		for(int j = 0; j < PRINT_MAX_WIDTH; ++j)
 		{
 			print_buffer[i*PRINT_MAX_WIDTH + j] = sprites[EMPTY];
@@ -159,18 +264,21 @@ print_fillGameScreen(u8* map, u8* print_buffer, int map_height, int map_width)
 {
 	for(int i = 0; i < map_height; ++i) 
 	{
-		for(int  j = 0; j < map_width; ++j) print_buffer[i*PRINT_MAX_WIDTH + j] = sprites[map[i*map_width + j]];
+		for(int  j = 0; j < map_width; ++j) print_buffer[i*PRINT_MAX_WIDTH + j] = sprites[map[i*map_width + j]]; //map[i*map_width + j] + ' ';
 	}
 }
 
 int
 main(int argc, int** argv)
 {
-	int map_height = MAP_MAX_HEIGHT/2;
-	int map_width = MAP_MAX_WIDTH/2;
+	int multDiv = 1;
+	int multDen = 1;
+	int map_height = MAP_MAX_HEIGHT*multDiv/multDen;
+	int map_width = MAP_MAX_WIDTH*multDiv/multDen;
 	int print_height = map_height;
 	int print_width = map_width + 1;
-	
+	int room_minAttempts = 15*multDiv/multDen;	
+	int room_maxAttempts = 30*multDiv/multDen;	
 	
 	u8 print_buffer[PRINT_MAX_HEIGHT * PRINT_MAX_WIDTH + 1] = {0};
 	u8 map[MAP_MAX_HEIGHT * MAP_MAX_WIDTH] = {0};
@@ -181,9 +289,15 @@ main(int argc, int** argv)
 	
 
 	//map_makeRoom(map, 0, 0, MAP_HEIGHT, MAP_WIDTH, 1); 
-	map_generateRooms(map, map_height, map_width, 7, 15);
+	map_generateRooms(map, map_height, map_width, room_minAttempts, room_maxAttempts);
 
 	print_fillGameScreen(map, print_buffer, map_height, map_width);	
+	printf("%s", print_buffer);
+	
+	map_makeDoorways(map, map_height, map_width);
+	//map_fillRooms(map, map_height, map_width);
+	print_fillGameScreen(map, print_buffer, map_height, map_width);	
+
 	printf("%s", print_buffer);
 }
 
